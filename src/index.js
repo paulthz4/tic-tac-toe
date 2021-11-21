a1e18e8aa31f4e18a810a3b17bbb5b4447c4749d
paulthz4 <43190631+paulthz4@users.noreply.github.com>
2021-11-21 15:51:17 -0700

    taking turns

--- src/index.js
+++ src/index.js
@@ -31,7 +31,7 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: '+(this.state.xIsNext ? 'X' : 'O');
 
     return (
       <div>
